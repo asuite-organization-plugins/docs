bf262d2ce4b304438fa11b1738fe27d8b0453809
JeraldRobinson <32209470+JeraldRobinson@users.noreply.github.com>
2017-10-17 11:48:57 -0500

    Update and rename docs.php to drive.php

--- drive.php
+++ drive.php
@@ -1,19 +1,19 @@
 <?php
 /*
-Plugin Name:  ASuite Docs
-Plugin URI:   http://docs.asuite.cf
-Description:  Enables the ASuite Docs app.
+Plugin Name:  ASuite Drive
+Plugin URI:   http://drive.asuite.cf
+Description:  Enables the ASuite Drive app.
 Version:      1.0
 Author:       ASuite
 Author URI:   https://portal.asuite.cf
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  asuite-docs
+Text Domain:  asuite-drive
 */
 
 function wpse_load_plugin_css() {
     $plugin_url = plugin_dir_url( __FILE__ );
 
-    wp_enqueue_style( 'style1', $plugin_url . 'docs-stylesheet.css' );
+    wp_enqueue_style( 'style1', $plugin_url . 'drive-stylesheet.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
